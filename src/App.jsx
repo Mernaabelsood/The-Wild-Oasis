3e66d0d8aef740ee3b2a9cd3424294010c5d477a
Mernaabelsood <merna217@gmail.com>
2025-03-09 11:34:16 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -33,15 +33,16 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="/dashboard" />} />
+            <Route index element={<Navigate replace to="/cabins" />} />
+            {/* <Route index element={<Navigate replace to="/dashboard" />} /> */}
             {/* <Route path="/dashboard" element={<Dashboard />} /> */}
             {/* <Route path="/bookings" element={<Bookings />} /> */}
             {/* <Route path="/users" element={<Users />} /> */}
             <Route path="/cabins" element={<Cabins />} />
             {/* <Route path="/settings" element={<Settings />} /> */}
-            <Route path="/account" element={<Account />} />
+            {/* <Route path="/account" element={<Account />} /> */}
           </Route>
-          <Route path="/login" element={<Login />} />
+          {/* <Route path="/login" element={<Login />} /> */}
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
